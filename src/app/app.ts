3860b87b817dafaa27da1665f1278cf8a475cdfa
unknown <g00469622@atu.ie>
2026-02-25 11:37:55 +0000

    Lab 4 Complete

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,39 @@
+//imports
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ListComponent } from './list-component/list-component';
 
+//angular component
 @Component({
+  //specifies css selector
   selector: 'app-root',
-  imports: [RouterOutlet],
+  //imports component uses
+  imports: [RouterOutlet, ListComponent],
+  //html template
   templateUrl: './app.html',
+  //css styles
   styleUrl: './app.css'
 })
+
 export class App {
-  protected readonly title = signal('data-binding-app');
+  //declare variables
+  hide:boolean = false;
+  count:number = 0;
+
+  //function to increase count on button click
+  onButtonClick(){
+    //increases count by 1
+    this.count++;
+  }
+
+  //function to hide text on star double clicked
+  onImageDoubleClick(){
+    //if star is hidden when double clicked
+    if(this.hide == true){
+      this.hide = false;
+    }else{ //else if show when double clicked hide
+      this.hide = true;
+    }
+    
+  }
 }
